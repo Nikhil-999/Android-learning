62f09fd52844fceb5a8617545ffb7f66b876d7d0
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-03 12:38:32 +0530

    Changes in fragment project

---
+++ Development/Project 4 Fragments/app/src/main/java/com/example/fragmentapp/Fragments/FirstFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fragmentapp.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentapp.R
+
+
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ Development/Project 4 Fragments/app/src/main/java/com/example/fragmentapp/Fragments/SecondFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fragmentapp.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentapp.R
+
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
