bcb74aa4a3861d939d350be717a60353656ea0af
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-03 11:38:33 +0530

    New project for fragments

--- Development/Project 3 Bitmap/app/src/main/java/com/example/imageconverter/MainActivity.kt
+++ Development/Project 3 Bitmap/app/src/main/java/com/example/imageconverter/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.imageconverter
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun myfun(view: View) {
+
+        val image = findViewById<ImageView>(R.id.myImage)
+        val myDrawable = image.drawable as BitmapDrawable
+        val mybitmap = myDrawable
+        val image2 = findViewById<ImageView>(R.id.myImage2)
+        image2.setImageBitmap(mybitmap)
     }
 }
