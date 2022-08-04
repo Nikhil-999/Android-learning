c25a149d10124cf469e512af403dcc9b3882a437
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-04 13:05:42 +0530

    new updates in project 4

--- Development/Project 4 Fragments/app/src/main/java/com/example/fragmentapp/MainActivity.kt
+++ Development/Project 4 Fragments/app/src/main/java/com/example/fragmentapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.fragmentapp
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.fragmentapp.Fragments.FirstFragment
 import com.example.fragmentapp.Fragments.SecondFragment
@@ -15,22 +17,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.btnFrameOne).setOnClickListener {
+            myfun(FirstFragment(),  0 , 0)
+        }
+
+        findViewById<Button>(R.id.btnFrameTwo).setOnClickListener {
+            myfun(SecondFragment() , 1 , 1)
+        }
+
     }
 
-    fun firstButtonClicked(view: View) {
+    private fun myfun(fragment : Fragment , myflag : Int , a : Int ){
+        val myManager = supportFragmentManager
+        val mytransaction = myManager.beginTransaction()
 
-        val demo = FirstFragment()
-        val mytransaction = supportFragmentManager.beginTransaction()
-        mytransaction.replace(R.id.fragmentContainer , demo)
+//        if(myflag == 0)
+//            mytransaction.add(R.id.fragmentContainer , fragment)
+//        else
+        if(myflag == 0){
+            supportFragmentManager.findFragmentById(a+1)?.let { mytransaction.detach(it) }
+        }
+            mytransaction.replace(R.id.fragmentContainer , fragment , )
+
+        mytransaction.addToBackStack(null)
+        supportFragmentManager.popBackStack(0,1)
         mytransaction.commit()
-
-
     }
-    fun secondButtonCLicked(view: View) {
-        val demo = SecondFragment()
-        val mytransaction = supportFragmentManager.beginTransaction()
-        mytransaction.replace(R.id.fragmentContainer , demo)
-        mytransaction.commit()
-    }
+
 
 }
