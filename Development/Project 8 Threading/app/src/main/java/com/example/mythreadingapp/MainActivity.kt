191ec3f3620dc25ff1edf2446d529c0d5b129272
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-08 18:22:10 +0530

    changes in the project threading

--- Development/Project 8 Threading/app/src/main/java/com/example/mythreadingapp/MainActivity.kt
+++ Development/Project 8 Threading/app/src/main/java/com/example/mythreadingapp/MainActivity.kt
@@ -8,20 +8,28 @@ import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var button: Button
     private var mainHandler: Handler = Handler()
+    private var check: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.StartButton)
+        button.setBackgroundColor(Color.BLACK)
     }
 
     fun startThreading(view: View) {
+        check = false
+        button.setBackgroundColor(Color.BLACK)
+        button.text = "Start Threading"
+        Snackbar.make(button , "Threading has started" , Snackbar.LENGTH_LONG ).show()
         //Below is Calling the thread method using Thread class.
 //        val demo = MyThread()
 //        demo.start()
@@ -30,9 +38,25 @@ class MainActivity : AppCompatActivity() {
         val myRunVar = MySecondThread(15)
         val demo2 = Thread(myRunVar)
         demo2.start()
+
+        Thread(
+            Runnable {
+                run {
+                    for(i in 10..12){
+                        Log.e("helo" , "My helo message")
+                    }
+                    Handler(Looper.getMainLooper()).post(Runnable {             //Again Looper.getMainLooper is necessary or else it was giving error as this is the thread which is calling a view i.e a toast without a Looper.
+                        run {
+                            Toast.makeText(applicationContext, "This is another method of calling thread" , Toast.LENGTH_LONG).show()
+                        }
+                    })
+                }
+            }
+        ).start()
     }
     fun stopThreading(view: View) {
-
+        Snackbar.make(button , "Threading has Stopped" , Snackbar.LENGTH_LONG).show()
+        check = true
     }
 
     // Below is creating a Thread by using Thread Class
@@ -49,6 +73,9 @@ class MainActivity : AppCompatActivity() {
     inner class MySecondThread(var timer: Int) : Runnable {
         override fun run() {
             for(i in 0..timer){
+                if(check){
+                    return
+                }
                 if(i == 3){
                     val insideHandler = Handler(Looper.getMainLooper())           // Looper.getMainLooper() method had to be passed to constructor or else it gives error.
                     insideHandler.post(
