1cfe95502562fb6a234cb5e452e6be71dbfe80a9
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-17 11:32:02 +0530

    changes

--- AndroidJetpackDevelopment/Project1/app/src/main/java/com/example/firstcomposeapp/ui/theme/Theme.kt
+++ AndroidJetpackDevelopment/Project1/app/src/main/java/com/example/firstcomposeapp/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun FirstComposeAppTheme(
+fun FirstComposeAPPTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
