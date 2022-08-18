0005f4b3531ee4a94979f1bb462b06dcb1f22f8b
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-18 11:13:29 +0530

    new project on DataBinding

--- AndroidJetpackDevelopment/Project3LiveData/app/src/main/java/com/example/mylivedataapp/MainActivity.kt
+++ AndroidJetpackDevelopment/Project3LiveData/app/src/main/java/com/example/mylivedataapp/MainActivity.kt
@@ -4,30 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var myViewModelClass: MyViewModelClass
-    lateinit var myTextView: TextView
-    lateinit var mybtn: Button
+    private lateinit var myViewModelClass: MyViewModelClass
+    private lateinit var myTextView: TextView
+    private lateinit var mybtn1: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mybtn = findViewById<Button>(R.id.btnClickMe)
+        mybtn1 = findViewById(R.id.btnClickMe)
         myTextView = findViewById<Button>(R.id.myTextView)
 
         myViewModelClass = ViewModelProvider(this).get(MyViewModelClass::class.java)
 
         //below observer is constantly monitoring the MutableLiveData, as the string inside it updates by any action, then the observer it alerted and do the action.
-        myViewModelClass.myLiveData.observe(this , Observer {
+        myViewModelClass.myLiveData.observe(this) {
             myTextView.text = it
-        })
+        }
 
-        mybtn.setOnClickListener {
+        mybtn1.setOnClickListener {
             myViewModelClass.updateLiveData()
 
             // myViewModelClass.myLiveData.value = "2 seconds have passed"
