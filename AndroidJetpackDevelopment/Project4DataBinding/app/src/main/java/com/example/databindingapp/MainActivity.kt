36a08c71ab5cf2552c12ed1a8ae541a7f223a8b2
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-18 12:00:31 +0530

    dataBinding project completed successfully

--- AndroidJetpackDevelopment/Project4DataBinding/app/src/main/java/com/example/databindingapp/MainActivity.kt
+++ AndroidJetpackDevelopment/Project4DataBinding/app/src/main/java/com/example/databindingapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.databindingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myDataClass: MyDataClass
+    private lateinit var myBinder: ActivityMainBinding      //this class is automatically created when we set the XML layout into <layout> .... </layout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        myBinder = DataBindingUtil.setContentView(this , R.layout.activity_main)   //this dataBindingUtil method is important to bind the Content views.
+
+        myDataClass = MyDataClass("Binding done using a Variable" , "Binding done using a Variable")
+
+//        myBinder.headerID.text = "this is Binding using ID calling Header"        //--> this is without using data class AND variable
+//        myBinder.footerID.text = "This is Bindgin using ID calling Footer"        //--> this is without using data class AND variable
+
+        myBinder.btnCLickMe.setOnClickListener {
+            myBinder.myVar = myDataClass            //as myVar is the type of myDataClass hence it can hold the reference to this class.
+        }
+
     }
+
+
 }
---
+++ AndroidJetpackDevelopment/Project4DataBinding/app/src/main/java/com/example/databindingapp/MyDataClass.kt
@@ -0,0 +1,3 @@
+package com.example.databindingapp
+
+data class MyDataClass(var header: String, var footer: String)
