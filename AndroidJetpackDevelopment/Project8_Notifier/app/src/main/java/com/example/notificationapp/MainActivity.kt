1f2e122eef20450ec43d3d72b38752e27b3a2c2e
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-22 17:45:04 +0530

    succesfull completion of notification APP

--- AndroidJetpackDevelopment/Project8_Notifier/app/src/main/java/com/example/notificationapp/MainActivity.kt
+++ AndroidJetpackDevelopment/Project8_Notifier/app/src/main/java/com/example/notificationapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.notificationapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNotifyMe: Button = findViewById(R.id.btnNotifyMe)
+
+        btnNotifyMe.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    "1",
+                    "channel MyName",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ).apply {
+                    description = "this is description"
+                }
+                // Register the channel with the system
+                val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+
+
+            val builder = NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("First Notification")
+                .setContentText("First Ever Successful notification.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(1, builder.build())
+            }
+        }
     }
+
 }
+
+
