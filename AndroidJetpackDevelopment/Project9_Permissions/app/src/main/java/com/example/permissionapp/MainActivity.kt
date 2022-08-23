230694d6071afb70b7f4cd65830706b50cd434ea
Nikhil Singh <iamnikhil2409@gmail.com>
2022-08-23 10:39:23 +0530

    #

--- AndroidJetpackDevelopment/Project9_Permissions/app/src/main/java/com/example/permissionapp/MainActivity.kt
+++ AndroidJetpackDevelopment/Project9_Permissions/app/src/main/java/com/example/permissionapp/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.permissionapp
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val myPermissions = arrayOf("android.permission.WRITE_EXTERNAL_STORAGE" , "android.permission.CALL_PHONE")
+    private lateinit var btnPermission: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPermission = findViewById(R.id.btnPermission)
+
+        btnPermission.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(myPermissions , 80)
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 80){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted" , Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "Permission Denied" , Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
